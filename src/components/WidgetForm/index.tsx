f02da0e7e39fc4979fd113eb5f15dfd5273fe3c0
iquecode <iquecode@gmail.com>
2022-05-03 23:59:16 -0300

    stage 2/5 ok

--- src/components/WidgetForm/index.tsx
+++ src/components/WidgetForm/index.tsx
@@ -5,6 +5,8 @@ import ideaImage from '../../assets/idea.svg'
 import thoughtImage from '../../assets/thought.svg'
 import { useState } from "react";
 import { FeedbackTypeStep } from "./Steps/FeedbackTypeStep";
+import { FeedbackContentStep } from "./Steps/FeedbackContentStep";
+import { FeedbackSuccessStep } from "./Steps/FeedbackSuccessStep";
 
 export const feedbackTypes = {
     BUG: {
@@ -35,31 +37,34 @@ export type FeedbackType = keyof typeof feedbackTypes
 export function WidgetForm() {
 
     const [feedbackType, setFeedbackType] = useState<FeedbackType | null>(null)
+    const [feedbackSent, setFeedbackSent] = useState(false);
+   
+    function handleRestartFeedback() {
+        setFeedbackSent(false);
+        setFeedbackType(null);
+    }
 
     return (
         <div className="bg-zinc-900 p-4 relative rounded-2xl mb-4 flex flex-col items-center shadow-lg w-[calc(100vw-2rem)] md:w-auto">
-            
-            <header>
-                <span className="text-xl leading-6" >Deixe seu feedback</span>
-                <CloseButton />
-            </header>
-
-
-            {!feedbackType ? (
-                <FeedbackTypeStep onFeedbackTypeChanged={setFeedbackType} />
-                
-
+            { feedbackSent ? (
+                <FeedbackSuccessStep onFeedbackRestartRequested={handleRestartFeedback} />
             ) : (
-
-
-                <p>Hello World</p>
-
-
+                <>
+                {!feedbackType ? (
+                <FeedbackTypeStep 
+                    onFeedbackTypeChanged={setFeedbackType} 
+                />
+                ) : (
+                    <FeedbackContentStep 
+                        feedbackType={feedbackType} 
+                        onFeedbackRestartRequested={handleRestartFeedback}
+                        onFeedbackSent={()=>setFeedbackSent(true)}
+                    />
+                )}
+                </>
             )}
 
-            
-
-            <footer className="text-xs text-neutral-400">
+            <footer className="text-xs text-neutral-400 mt-4">
                 Feito com ♥ por <a className="underline underline-offset-2" href="https://ique.dev">ique.dev</a>
             </footer>
 
